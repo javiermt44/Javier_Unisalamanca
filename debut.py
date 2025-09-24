91a6798cf5f0f1182496ee461288f4bf59b7b2d2
javiermt <javiermt44@gmail.com>
2025-09-23 21:37:36 -0500

    agregar multiplicacion

--- debut.py
+++ debut.py
@@ -2,4 +2,6 @@ print ("hola mundo")
 numero1=57
 numero2=75
 resultado=numero1 / numero2
+resultado1=numero1 * numero2
 print ("la division de numero1 y numero2 es", resultado)
+print ("la multiplicacion de nuemro1 y numero2 es", resultado1)
