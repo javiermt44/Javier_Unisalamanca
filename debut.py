f8ff72f89b29450feb31b559146854b2d51dad2b
javiermt <javiermt44@gmail.com>
2025-09-23 21:45:53 -0500

    agregar edad.

--- debut.py
+++ debut.py
@@ -3,5 +3,7 @@ numero1=57
 numero2=75
 resultado=numero1 / numero2
 resultado1=numero1 * numero2
+edad=57
 print ("la division de numero1 y numero2 es", resultado)
 print ("la multiplicacion de nuemro1 y numero2 es", resultado1)
+print ("mi edad es", edad, "años")
